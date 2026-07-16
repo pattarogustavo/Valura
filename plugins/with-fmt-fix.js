222608e79f2e0b248307cf575857eb6330082ef7
GHStudio <pattaro.gustavo@gmail.com>
2026-07-16 23:11:58 +0200

    Update Podfile to include fmtPatch in post_install
    Insert fmtPatch into existing post_install block in Podfile.

--- plugins/with-fmt-fix.js
+++ plugins/with-fmt-fix.js
@@ -11,21 +11,26 @@ module.exports = function withFmtFix(config) {
         'Podfile'
       );
       let content = fs.readFileSync(podfilePath, 'utf8');
-      if (content.includes('# withFmtFix')) return config;
-      content += `
-# withFmtFix
-post_install do |installer|
-  installer.pods_project.targets.each do |target|
-    if target.name == 'fmt'
-      target.build_configurations.each do |cfg|
-        cfg.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
-        cfg.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-        cfg.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_USE_CONSTEVAL=0'
+
+      if (content.includes('FMT_USE_CONSTEVAL')) return config;
+
+      const fmtPatch = `
+    installer.pods_project.targets.each do |target|
+      if target.name == 'fmt'
+        target.build_configurations.each do |cfg|
+          cfg.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
+          cfg.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
+          cfg.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_USE_CONSTEVAL=0'
+        end
       end
-    end
-  end
-end
-`;
+    end`;
+
+      // Insere DENTRO do post_install existente, antes do 'end' final
+      content = content.replace(
+        /post_install do \|installer\|([\s\S]*?)^end/m,
+        `post_install do |installer|$1${fmtPatch}\nend`
+      );
+
       fs.writeFileSync(podfilePath, content);
       return config;
     },
