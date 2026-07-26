e9c78f1453c0a347bb16015559507dd235135a12
GHStudio <pattaro.gustavo@gmail.com>
2026-07-26 22:09:24 +0200

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,13 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
   stream: require.resolve('stream-browserify'),
+  zlib: require.resolve('browserify-zlib'),
+  path: require.resolve('path-browserify'),
+  crypto: require.resolve('crypto-browserify'),
+  http: require.resolve('stream-http'),
+  https: require.resolve('https-browserify'),
+  os: require.resolve('os-browserify/browser'),
+  url: require.resolve('url'),
 };
 
 module.exports = config;
