4fbf32b642ec7723df1414f788b132eea33b0477
GHStudio <pattaro.gustavo@gmail.com>
2026-07-26 23:00:09 +0200

    Add WebSocket mock implementation in index.js

---
+++ shims/ws-mock/index.js
@@ -0,0 +1,12 @@
+'use strict';
+const W = global.WebSocket;
+class WebSocketServer {
+  constructor() {}
+  on() {}
+  close() {}
+}
+module.exports = W;
+module.exports.WebSocket = W;
+module.exports.WebSocketServer = WebSocketServer;
+module.exports.createWebSocketStream = () => {};
+module.exports.default = W;
