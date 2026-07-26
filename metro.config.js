72ce59aa33297ff3a6b1acbfd8e3bfd35aaab42f
GHStudio <pattaro.gustavo@gmail.com>
2026-07-26 22:16:44 +0200

    Add shims for net, tls, fs, and dns modules

--- metro.config.js
+++ metro.config.js
@@ -3,14 +3,19 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
-  stream: require.resolve('stream-browserify'),
-  zlib: require.resolve('browserify-zlib'),
-  path: require.resolve('path-browserify'),
-  crypto: require.resolve('crypto-browserify'),
-  http: require.resolve('stream-http'),
-  https: require.resolve('https-browserify'),
-  os: require.resolve('os-browserify/browser'),
-  url: require.resolve('url'),
+  ws:      require.resolve('./shims/ws'),
+  stream:  require.resolve('stream-browserify'),
+  zlib:    require.resolve('browserify-zlib'),
+  path:    require.resolve('path-browserify'),
+  crypto:  require.resolve('crypto-browserify'),
+  http:    require.resolve('stream-http'),
+  https:   require.resolve('https-browserify'),
+  os:      require.resolve('os-browserify/browser'),
+  url:     require.resolve('url'),
+  net:     require.resolve('./shims/empty'),
+  tls:     require.resolve('./shims/empty'),
+  fs:      require.resolve('./shims/empty'),
+  dns:     require.resolve('./shims/empty'),
 };
 
 module.exports = config;
