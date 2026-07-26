10dbf0026166f7840f6972f1898bac50c76d8416
GHStudio <pattaro.gustavo@gmail.com>
2026-07-26 23:01:53 +0200

    Refactor metro.config.js to simplify resolver
    Removed custom WebSocket resolution and unused shims.

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Intercepta ws ANTES da resolução normal (extraNodeModules não funciona para pacotes instalados)
-const originalResolveRequest = config.resolver.resolveRequest;
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName === 'ws' || moduleName.startsWith('ws/')) {
-    return {
-      filePath: path.resolve(__dirname, './shims/ws.js'),
-      type: 'sourceFile',
-    };
-  }
-  if (originalResolveRequest) {
-    return originalResolveRequest(context, moduleName, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
-
-// Polyfills para módulos built-in do Node.js (estes SIM funcionam com extraNodeModules)
 config.resolver.extraNodeModules = {
   stream:  require.resolve('stream-browserify'),
   zlib:    require.resolve('browserify-zlib'),
@@ -30,11 +13,6 @@ config.resolver.extraNodeModules = {
   url:     require.resolve('url'),
   util:    require.resolve('util'),
   events:  require.resolve('events'),
-  net:     require.resolve('./shims/empty'),
-  tls:     require.resolve('./shims/empty'),
-  fs:      require.resolve('./shims/empty'),
-  dns:     require.resolve('./shims/empty'),
-  assert:  require.resolve('./shims/empty'),
 };
 
 module.exports = config;
