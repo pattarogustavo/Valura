89beb3d8a6eec71591b2e842d12baf7375d3c6d2
GHStudio <pattaro.gustavo@gmail.com>
2026-07-26 23:15:06 +0200

    Add WebSocket shim for React Native
    Implement a WebSocket shim for React Native with a fallback to a mock WebSocketServer if native WebSocket is not available.

--- shims/ws.js
+++ shims/ws.js
@@ -1,3 +1,26 @@
-const W3CWebSocket = global.WebSocket;
-module.exports = W3CWebSocket;
-module.exports.w3cwebsocket = W3CWebSocket;
+'use strict';
+// React Native has native WebSocket support
+// This shim replaces the Node.js 'ws' package
+const NativeWebSocket = global.WebSocket;
+
+class WebSocketServer {
+  constructor() {}
+  on() { return this; }
+  once() { return this; }
+  emit() { return this; }
+  close(cb) { if (typeof cb === 'function') cb(); }
+  address() { return {}; }
+  get clients() { return new Set(); }
+}
+
+if (NativeWebSocket) {
+  NativeWebSocket.Server = WebSocketServer;
+  NativeWebSocket.WebSocket = NativeWebSocket;
+  NativeWebSocket.WebSocketServer = WebSocketServer;
+  NativeWebSocket.createWebSocketStream = () => null;
+  module.exports = NativeWebSocket;
+} else {
+  module.exports = WebSocketServer;
+  module.exports.WebSocket = WebSocketServer;
+  module.exports.WebSocketServer = WebSocketServer;
+}
