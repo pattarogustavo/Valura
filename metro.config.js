f2ac4e35e816e31db855181e6272ae5c1b2868de
GHStudio <pattaro.gustavo@gmail.com>
2026-07-26 22:26:14 +0200

    Modify metro.config.js to intercept 'ws' resolution
    Intercepts the resolution of 'ws' module to use a custom shim and adds additional polyfills for Node.js built-in modules.

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,25 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
+// Intercepta ws ANTES da resolução normal (extraNodeModules não funciona para pacotes instalados)
+const originalResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'ws' || moduleName.startsWith('ws/')) {
+    return {
+      filePath: path.resolve(__dirname, './shims/ws.js'),
+      type: 'sourceFile',
+    };
+  }
+  if (originalResolveRequest) {
+    return originalResolveRequest(context, moduleName, platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+// Polyfills para módulos built-in do Node.js (estes SIM funcionam com extraNodeModules)
 config.resolver.extraNodeModules = {
-  ws:      require.resolve('./shims/ws'),
   stream:  require.resolve('stream-browserify'),
   zlib:    require.resolve('browserify-zlib'),
   path:    require.resolve('path-browserify'),
@@ -12,10 +28,13 @@ config.resolver.extraNodeModules = {
   https:   require.resolve('https-browserify'),
   os:      require.resolve('os-browserify/browser'),
   url:     require.resolve('url'),
+  util:    require.resolve('util'),
+  events:  require.resolve('events'),
   net:     require.resolve('./shims/empty'),
   tls:     require.resolve('./shims/empty'),
   fs:      require.resolve('./shims/empty'),
   dns:     require.resolve('./shims/empty'),
+  assert:  require.resolve('./shims/empty'),
 };
 
 module.exports = config;
